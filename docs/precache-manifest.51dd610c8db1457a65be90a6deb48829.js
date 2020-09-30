73356bc5db688c2b520c43c4bdd08cca9d99d296
Javier <fcobo9608@gmail.com>
2020-09-29 23:26:12 -0500

    docs

---
+++ docs/precache-manifest.51dd610c8db1457a65be90a6deb48829.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9978d984abe9aa34a69b6b8751166bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb05739659e49cd546f0",
+    "url": "/static/js/2.c3c976bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c3c976bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93dae6ec62289c427f5c",
+    "url": "/static/js/main.deb7febf.chunk.js"
+  },
+  {
+    "revision": "4275e298ca4247238cbd",
+    "url": "/static/js/runtime-main.a18a15fd.js"
+  }
+]);
